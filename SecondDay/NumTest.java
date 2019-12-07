4c1d26e016ec2713878742aabc7cfeb7804a3de2
88-Rising <348514635@qq.com>
2019-12-08 00:25:13 +0800

    守形数

--- SecondDay/NumTest.java
+++ SecondDay/NumTest.java
@@ -1,5 +1,7 @@
 package SecondDay;
 import java.util.Scanner;
+//守形数是这样一种整数，它的平方的低位部分等于它本身。 比如25的平方是625，低位部分是25，因此25是一个守形数。
+// 编一个程序，判断N是否为守形数。
 public class NumTest{
         public static void main(String[] args){
             Scanner sc=new Scanner(System.in);
