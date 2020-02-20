9f4d60398782bf48b39cca1c4ea5d05e5bded1ea
88-Rising <348514635@qq.com>
2020-02-20 18:03:31 +0800

    寻找第K大的元素

---
+++ FindNum.java
@@ -0,0 +1,44 @@
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class FindNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] str01 = (str.replace("[", "").
+                replace("]", "")).split(",");
+        int[] nums = new int[str01.length];
+        List list=new ArrayList();
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(str01[i]);
+        }
+        Arrays.sort(nums);
+        for(int j=0;j<nums.length-1;j++){
+            if(nums[j]!=nums[j+1]){
+                list.add(nums[j]);
+
+            }
+        }
+        list.add(nums[nums.length-1]);
+//        for(int i = 0;i < list.size();i++){
+//            System.out.println(list.get(i));
+//        }
+//       if(list.size()<5){
+//           System.out.println(list.get(2));
+//       }else{
+//           System.out.println(list.get(list.size()-2));
+//       }
+
+        if(nums.length<2){
+            System.out.println(-1);
+        }else{
+            System.out.println(nums[nums.length-3]);
+        }
+    }
+
+}
+
