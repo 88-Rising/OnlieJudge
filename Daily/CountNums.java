94fdd66f219a606213a23c18d594852281f1617e
88-Rising <348514635@qq.com>
2020-02-21 17:04:24 +0800

    使用HashMap解决给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标

--- Daily/CountNums.java
+++ Daily/CountNums.java
@@ -1,6 +1,7 @@
 package Daily;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 //给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 //
@@ -10,21 +11,52 @@ import java.util.Arrays;
 //        链接：https://leetcode-cn.com/problems/two-sum
 //        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 public class CountNums {
-
-        public int[] twoSum(int[] nums, int target) {
-            int[] num=new int[2];
-            for(int i=0;i<nums.length;i++){
-                for(int j=i+1;j<nums.length;j++){
-                    if(nums[i]+nums[j]==target){
-                        num[0]=i;
-                        num[1]=j;
-                        break;
-                    }
-                }
+//
+//        public int[] twoSum(int[] nums, int target) {
+//            int[] num=new int[2];
+//            for(int i=0;i<nums.length;i++){
+//                for(int j=i+1;j<nums.length;j++){
+//                    if(nums[i]+nums[j]==target){
+//                        num[0]=i;
+//                        num[1]=j;
+//                        break;
+//                    }
+//                }
+//            }
+//
+//            return num;
+//        }
+//    public int[] oneSum(int[] nums,int target){
+//        HashMap<Integer,Integer> map=new HashMap<>();
+//        int[] res = new int[2];
+//        for(int i=0;i<nums.length;i++){
+//            int dif=target-nums[i];
+//            if(map.get(dif)!=null){
+//                res[0]=i;
+//                res[1]=map.get(dif);
+//            }
+//            map.put(nums[i],i);
+//
+//        }
+//        return res;
+//
+//    }
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int dif = target - nums[i]; //记录每一个数字和target的差值  dif是作为key值
+            if (map.get(dif) != null) { //在哈希表中查找value值 如果有相同的就返回这个值和当前数组的下标
+                res[0] = map.get(dif);
+                res[1] = i;
+                return res;
             }
-
-            return num;
+            map.put(nums[i],i);
         }
+        return res;
+    }
+
+
 
     public static void main(String[] args) {
 //        利用Array类中的toString方法
