a0a3b2d1ed969126ce4429ca1083f24dc18af21c
88-Rising <348514635@qq.com>
2020-03-02 17:02:11 +0800

    使用二分查找解决输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。

--- Swordquest/Swordquest6.java
+++ Swordquest/Swordquest6.java
@@ -16,4 +16,27 @@ public class Swordquest6 {
         }
         return key;
     }
+    public int minNumberInRotateArray1(int [] array) {
+        if(array.length==0){
+            return 0;
+        }
+        int left=0;
+        int right=array.length-1;
+        int mid=-1;
+        while(array[left]>=array[right]){
+              if(right-left==1){
+                  mid=right;
+                  break;
+              }
+              mid=left+(right-left)/2;
+              if(array[left]<=array[mid]){
+                  left=mid;
+              }
+              if(array[right]>=array[mid]){
+                  right=mid;
+              }
+        }
+        return array[mid];
+
+    }
 }
