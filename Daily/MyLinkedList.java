e5d9990c2a829c56a82bcc4d2cb68770fa835212
88-Rising <348514635@qq.com>
2020-03-18 23:59:59 +0800

    链表尾插

--- Daily/MyLinkedList.java
+++ Daily/MyLinkedList.java
@@ -10,16 +10,47 @@ class Node{
 public class MyLinkedList {
 
     public static void display(Node head){
-        for(Node cur=head;cur.next!=null;cur=cur.next){
+        for(Node cur=head;cur!=null;cur=cur.next){
             System.out.printf("(%d)->",cur.val);
         }
-        System.out.print("null%n");
+        System.out.print("null");
+        System.out.println();
     }
 
     public static Node  pushFront(Node head,int val){
         Node newNode =new Node(val);
         newNode.next=head;
         return newNode;
+
+    }
+    public static Node getLast(Node head){
+        if(head==null){
+            return null;
+        }
+        Node cur=head;
+        while(cur.next!=null){
+            cur=cur.next;
+        }
+        return cur;
+    }
+    public static Node pushBack(Node head,int val){
+        Node newNode=new Node(val);
+        if(head==null){
+            return newNode;
+        }
+        Node last=getLast(head);
+        last.next=newNode;
+        return head;
+
+
+    }
+
+    public static void main(String[] args) {
+        Node head=null;
+        head = pushFront(head,1);
+        head = pushFront(head,2);
+        head = pushFront(head,3);
+        display(head);
 
     }
 }
