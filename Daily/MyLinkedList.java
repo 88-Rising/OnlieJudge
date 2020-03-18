4f90c40de5c4ce766e98464cac94dcae4468e4cc
88-Rising <348514635@qq.com>
2020-03-18 23:47:47 +0800

    循环打印链表

--- Daily/MyLinkedList.java
+++ Daily/MyLinkedList.java
@@ -9,6 +9,13 @@ class Node{
 }
 public class MyLinkedList {
 
+    public static void display(Node head){
+        for(Node cur=head;cur.next!=null;cur=cur.next){
+            System.out.printf("(%d)->",cur.val);
+        }
+        System.out.print("null%n");
+    }
+
     public static Node  pushFront(Node head,int val){
         Node newNode =new Node(val);
         newNode.next=head;
