05b103d0e9cbadbe6875481fbfe61b1ead6d3673
88-Rising <348514635@qq.com>
2020-03-20 20:55:56 +0800

    二分查找算法

---
+++ Daily/BinarySearch.java
@@ -0,0 +1,29 @@
+package Daily;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] arry,int key){
+        int low=0;
+        int high=arry.length-1;
+        int mid;
+        while(low<=high){
+            mid=(high+low)/2;
+            if(arry[mid]==key){
+                return mid;
+            }else if(arry[mid]<key){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] array=new int[]{1,6,8,9,13,19,56};
+        int index=binarySearch(array,56);
+        System.out.println(index);
+    }
+}
