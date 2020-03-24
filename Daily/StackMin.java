38068e3ebe87a3c5ad84ea68f1cb4314bb142c79
88-Rising <348514635@qq.com>
2020-03-24 15:20:53 +0800

    自定义栈寻找最小元素

---
+++ Daily/StackMin.java
@@ -0,0 +1,39 @@
+package Daily;
+import java.util.Stack;
+public class StackMin {
+
+/*
+* 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
+注意：保证测试中不会当栈为空的时候，对栈调用pop()或者min()或者top()方法。
+* */
+        Stack<Integer> myStack1 = new Stack<>();
+        Stack<Integer> myStack2 = new Stack<>();
+        public void push(int node) {
+            myStack1.push(node);
+        }
+
+        public void pop() {
+            myStack1.pop();
+        }
+
+        public int top() {
+            return myStack1.peek();
+        }
+
+        public int min() {
+            int min = Integer.MAX_VALUE;
+            while(!myStack1.empty()){
+                int node=myStack1.pop();
+                if(node<min){
+                    min=node;
+                }
+                myStack2.push(node);
+            }
+            while(!myStack2.isEmpty()){
+                myStack1.push(myStack2.pop());
+            }
+            return min;
+
+        }
+
+}
