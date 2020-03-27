e4edcdc66f2a6c2fefa07e637556b3674f07bf6d
88-Rising <348514635@qq.com>
2020-03-27 17:02:37 +0800

    把一个二叉搜索树转换为一个双向链表(有序)

---
+++ Daily/ConvertBSTToBiList.java
@@ -0,0 +1,37 @@
+package Daily;
+
+import com.sun.deploy.panel.TreeRenderers;
+
+import java.util.Stack;
+
+public class ConvertBSTToBiList {
+    public static TreeNode convertBSTToBilist(TreeNode pRootOfTree){
+        if(pRootOfTree==null){
+            return null;
+        }
+        TreeNode pre=null;
+        TreeNode p=pRootOfTree;
+        boolean isFirst=true;
+        Stack<TreeNode> stack =new Stack<>();
+        while(p!=null||!stack.isEmpty()){
+            while(p!=null){
+                stack.push(p);
+                p=p.left;
+            }
+            p=stack.pop();
+            if(isFirst){
+                pRootOfTree=p;
+                pre=pRootOfTree;
+                isFirst=false;
+            }else{
+                pre.right=p;
+                p.left=pre;
+                pre=p;
+            }
+            p=p.right;
+
+        }
+
+        return pRootOfTree;
+    }
+}
