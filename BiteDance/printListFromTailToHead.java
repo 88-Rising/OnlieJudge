1091d07a668826dbabb92d398702c422752283ee
88-Rising <348514635@qq.com>
2020-07-23 10:45:16 +0800

    输入一个链表，按链表从尾到头的顺序返回一个ArrayList。//使用逆置数组解决

--- BiteDance/printListFromTailToHead.java
+++ BiteDance/printListFromTailToHead.java
@@ -10,7 +10,7 @@ import java.util.Stack;
 * */
 public class printListFromTailToHead {
 
-    public ArrayList<Integer> solution(ListNode listnode){
+    public ArrayList<Integer> solution(ListNode listnode){//借助栈来解决
 
         Stack<Integer> stack=new Stack<>();
         while(listnode!=null){
@@ -21,6 +21,26 @@ public class printListFromTailToHead {
         ArrayList<Integer> list=new ArrayList<>();
         while(!stack.empty()){
             list.add(stack.pop());
+        }
+        return list;
+    }
+
+    public ArrayList<Integer> solution1(ListNode listnode){//逆置数组
+
+        ArrayList<Integer> list=new ArrayList<>();
+        while(listnode!=null){
+            list.add(listnode.val);
+            listnode=listnode.next;
+        }
+
+        int i=0;
+        int j=list.size()-1;
+        while(i<j){
+            Integer temp=list.get(i);
+            list.set(i,list.get(j));
+            list.set(j,temp);
+            i++;
+            j--;
         }
         return list;
 
