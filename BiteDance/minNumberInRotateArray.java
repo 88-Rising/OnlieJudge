1a64ed133685301fdef45bdb646bda5abee8558f
88-Rising <348514635@qq.com>
2020-07-23 12:16:25 +0800

    在非递减旋转数组中找到最小值 使用遍历（优化方法）

--- BiteDance/minNumberInRotateArray.java
+++ BiteDance/minNumberInRotateArray.java
@@ -41,4 +41,19 @@ public class minNumberInRotateArray {
         return result;
 
     }
+
+    public int solution2(int[] array){
+        if(array==null||array.length==0){
+            return 0;
+        }
+
+        for(int i=0;i<array.length-1;i++){
+            if(array[i]>array[i+1]){
+                return array[i+1];
+            }
+        }
+
+        return array[0];
+
+    }
 }
