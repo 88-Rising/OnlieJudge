aa73fff8f635a7ae1b293645b6ac031ad5c6b584
88-Rising <348514635@qq.com>
2020-07-24 10:44:55 +0800

    数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组          {1,2,3,2,2,2,5,4,2}。由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。

---
+++ BiteDance/MoreThanHalfNum_Solution.java
@@ -0,0 +1,33 @@
+package BiteDance;
+
+import java.util.HashMap;
+
+/*
+*数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组
+{1,2,3,2,2,2,5,4,2}。由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+* */
+public class MoreThanHalfNum_Solution {
+
+    public static int solution(int[] array){
+        if(array==null){
+            return 0;
+        }
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<array.length;i++){
+            if(map.containsKey(array[i])){
+                int count=map.get(array[i]);
+                count++;
+                map.put(array[i],count);
+            }else{
+                map.put(array[i],1);
+
+            }
+            if(map.get(array[i])>array.length/2){
+                return array[i];
+            }
+        }
+        return 0;
+    }
+}
