ab0a86f6b737e890ba33fa6bc210e7e652d4d3f5
88-Rising <348514635@qq.com>
2020-07-26 11:43:38 +0800

    操作给定的二叉树，将其变换为源二叉树的镜像

---
+++ BiteDance/Mirror.java
@@ -0,0 +1,20 @@
+package BiteDance;
+/*
+* 操作给定的二叉树，将其变换为源二叉树的镜像
+* */
+public class Mirror {
+
+    public void mirror(TreeNode root){
+
+        if(root==null){
+            return;
+        }
+
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        mirror(root.left);
+        mirror(root.right);
+        return;
+    }
+}
