27136bd8a0d6b1e56dcd376e3aaef41109a1350b
88-Rising <348514635@qq.com>
2020-07-27 18:02:20 +0800

    输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。

---
+++ BiteDance/verifySquenceOfBST.java
@@ -0,0 +1,37 @@
+package BiteDance;
+/*
+*
+*
+输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+* */
+public class verifySquenceOfBST {
+
+    public boolean  VerifySquenceOfBSTCore(int[] seqence,int start,int end){
+        if(start>=end){
+            return true;
+        }
+        int root=seqence[end];
+        int i=0;
+        while(seqence[i]<root&&i<end){
+            i++;
+        }
+
+        for(int j=i;j<end;j++){
+            if(seqence[j]<root){
+                return false;
+            }
+
+        }
+        return VerifySquenceOfBSTCore(seqence,0,i-1)&&VerifySquenceOfBSTCore(seqence,i,end-1);
+
+    }
+
+    public boolean VerifySquenceOfBST(int[] sequence){
+        if(sequence.length==0){
+            return false;
+        }
+        return VerifySquenceOfBSTCore(sequence,0,sequence.length-1);
+    }
+
+
+}
