4659524de5d943e179aa2ac32529c88939ec7bb7
88-Rising <348514635@qq.com>
2020-08-01 11:38:48 +0800

    子串划分，子串逆置

--- BiteDance/ReverseSentence.java
+++ BiteDance/ReverseSentence.java
@@ -2,9 +2,10 @@ package BiteDance;
 
 import java.util.ArrayList;
 /*
-*汇编语言中有一种移位指令叫做循环左移（ROL），现在有个简单的任务，就是用字符串模拟这个指令
-的运算结果。对于一个给定的字符序列S，请你把其循环左移K位后的序列输出。例如，字符序列S=”
-abcXYZdef”,要求输出循环左移3位后的结果，即“XYZdefabc”。是不是很简单？OK，搞定它！
+*公司最近来了一个新员工Fish，每天早晨总是会拿着一本英文杂志，写些句子在本子上。同事Cat对Fish
+写的内容颇感兴趣，有一天他向Fish借来翻看，但却读不懂它的意思。例如，“student. a am I”。后来
+才意识到，这家伙原来把句子单词的顺序翻转了，正确的句子应该是“I am a student.”。Cat对一一的翻
+转这些单词顺序可不在行，你能帮助他么？
 *
 *  */
 public class ReverseSentence {
