9b4a5ee641b80a1ff3535ebfd1ade4b242ab587f
88-Rising <348514635@qq.com>
2020-08-01 17:07:03 +0800

    冒泡排序

---
+++ BiteDance/BubbleSort.java
@@ -0,0 +1,31 @@
+package BiteDance;
+
+import java.util.Arrays;
+/*
+* 冒泡排序法
+* */
+public class BubbleSort {
+    public static int[] bubbleSort(int[] array){
+        if(array.length==0){
+            return array;
+        }
+
+        for(int i=0;i<array.length-1;i++){
+            for(int j=0;j<array.length-1-i;j++){
+                if(array[j]>array[j+1]){
+                    int temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int [] array=new int[]{4,5,6,3,2,1};
+        int [] result=BubbleSort.bubbleSort(array);
+        System.out.println(Arrays.toString(result));
+    }
+
+}
