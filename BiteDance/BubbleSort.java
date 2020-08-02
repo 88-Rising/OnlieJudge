52670f5c2f14b81a05f4c3f9d0ced29c61de329e
88-Rising <348514635@qq.com>
2020-08-02 23:07:47 +0800

    冒泡排序

--- BiteDance/BubbleSort.java
+++ BiteDance/BubbleSort.java
@@ -9,7 +9,6 @@ public class BubbleSort {
         if(array.length==0){
             return array;
         }
-
         for(int i=0;i<array.length-1;i++){
             for(int j=0;j<array.length-1-i;j++){
                 if(array[j]>array[j+1]){
