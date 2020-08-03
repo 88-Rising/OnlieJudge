48cb7e5eab5049df5b72d729b873baeaff2e98b5
88-Rising <348514635@qq.com>
2020-08-03 11:38:50 +0800

    返回删除单链表的倒数第K个节点

--- BiteDance/findAndDelLastKNode.java
+++ BiteDance/findAndDelLastKNode.java
@@ -9,66 +9,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class findAndDelLastKNode {
-
-    public static class Node {
-
-        public int value;
-        public Node next;
-
-        public Node(int value) {
-            this.value = value;
+    public static void printLinkedList(ListNode head){
+        ListNode cur=head;
+        while(cur!=null){
+            System.out.print(cur.val);
+            cur=cur.next;
+            System.out.print(" ");
         }
+
     }
 
-    public static Node removeLastKthNode(Node head, int lastKth) {
-        if (head == null || lastKth < 1) {
+    public static ListNode removeLastKNode(int k,ListNode head){
+        if(k<1||head==null){
             return head;
         }
-        Node slow = head;
-        Node fast = head;
-        while (fast.next != null && lastKth > 0) {
-            fast = fast.next;
-            lastKth--;
+        ListNode fast=head;
+        ListNode slow=head;
+        while(k>0&&fast.next!=null){
+            fast=fast.next;
+            k--;
         }
-        if (lastKth == 1) {
-            head = head.next;
-        } else if (lastKth == 0) {
-            while (fast.next != null) {
-                fast = fast.next;
-                slow = slow.next;
+        if(k==1){
+            head=head.next;
+        }else if (k==0){
+            while(fast.next!=null){
+                fast=fast.next;
+                slow=slow.next;
             }
-            slow.next = slow.next.next;
         }
+        slow.next=slow.next.next;
         return head;
+
+
     }
 
-    public static Node listGenerator(int length, String[] numbers) {
-        Node head = new Node(Integer.parseInt(numbers[0]));
-        Node cur = head;
-        for (int i = 1; i < length; i++) {
-            cur.next = new Node(Integer.parseInt(numbers[i]));
-            cur = cur.next;
+    public static ListNode listGenerator(int n,String[] linkedList){
+        ListNode head=new ListNode(Integer.parseInt(linkedList[0]));
+        ListNode cur=head;
+        for(int i=1;i<linkedList.length;i++){
+            cur.next=new ListNode(Integer.parseInt(linkedList[i]));
+            cur=cur.next;
         }
-        cur.next = null;
+        cur.next=null;
         return head;
-    }
-
-    public static void printList(Node head) {
-        while (head != null) {
-            System.out.print(head.value +" ");
-            head = head.next;
-        }
-        System.out.println();
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String[] parameters = bufferedReader.readLine().split(" ");
-        int n = Integer.parseInt(parameters[0]);
-        int k = Integer.parseInt(parameters[1]);
-        String[] numbers = bufferedReader.readLine().split(" ");
-        Node head = listGenerator(n, numbers);
-        head = removeLastKthNode(head, k);
-        printList(head);
+        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
+        String [] lineFirst=bufferedReader.readLine().split(" ");
+        int n=Integer.parseInt(lineFirst[0]);
+        int k=Integer.parseInt(lineFirst[1]);
+        String[] linkedList=bufferedReader.readLine().split(" ");
+        ListNode head=listGenerator(n,linkedList);
+        ListNode result=removeLastKNode(k,head);
+        printLinkedList(result);
+
+
     }
 }
