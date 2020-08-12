734020ceae3ef5a6428ec8bd57c73461cce29887
88-Rising <348514635@qq.com>
2020-08-12 23:49:05 +0800

    在二维数组中查找目标值

--- JavaOnlineJudge/FindTargetInArray.java
+++ JavaOnlineJudge/FindTargetInArray.java
@@ -10,6 +10,7 @@ import java.io.*;
 public class FindTargetInArray{
 
     public static void main(String[] args) throws IOException{
+
         BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
         String[] lineFirst=bufferedReader.readLine().split(" ");
         int n=Integer.parseInt(lineFirst[0]);
