a6f575136d9190f1a9dd926e4f1e4e0c3c708c4a
88-Rising <348514635@qq.com>
2020-09-08 17:30:53 +0800

    ReviewReverseList

---
+++ LinLinLinkedList/ReverseList.java
@@ -0,0 +1,33 @@
+package LinLinLinkedList;
+
+import java.util.List;
+
+class ListNode{
+    int val;
+    ListNode next;
+    public ListNode(int val){
+        this.val=val;
+    }
+
+}
+
+public class ReverseList {
+
+    public ListNode reverseList(ListNode head){
+
+        if(head==null){
+            return null;
+        }
+
+        ListNode newList=null;
+        ListNode cur=head;
+
+        while(head!=null){
+            cur=head.next;
+            head.next=newList;
+            newList=head;
+            head=cur;
+        }
+        return newList;
+    }
+}
