225d1ad4965faab922fe18e9530a708628d21766
88-Rising <348514635@qq.com>
2020-09-10 10:51:07 +0800

    删除链表倒数第n个结点

--- LinLinLinkedList/RemoveNthFromEnd.java
+++ LinLinLinkedList/RemoveNthFromEnd.java
@@ -4,28 +4,27 @@ public class RemoveNthFromEnd {
 //删除链表的倒数第n个节点
 
     public ListNode removeNthFromEnd(ListNode head,int n){
-        if(head==null||n<=0){
-            return head;
-        }
+       if(n<=0||head==null){
+           return head;
+       }
 
-        ListNode fast=head;
-        ListNode slow=head;
-        while(n>0&&fast.next!=null){
-            fast=fast.next;
-            n--;
-        }
-        if(n==1){
-            return head;
-        }else if(n==0){
-            while(fast!=null&&fast.next!=null){
-            slow=slow.next;
-            fast=fast.next;
-            }
-            slow.next=slow.next.next;
-        }
+       ListNode fast=head;
+       ListNode slow=head;
+       while(n>0&&fast.next!=null){
+           fast=fast.next;
+           n--;
+       }
+       if(n==1){
+           head=head.next;
+       }else{
+           while(fast.next!=null){
+               fast=fast.next;
+               slow=slow.next;
+           }
+           slow.next=slow.next.next;
+       }
 
-
-        return slow;
+       return head;
 
     }
 }
